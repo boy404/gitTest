a74ecc313a30ea54c57573e7040a89439319d51c
Ljn <Ljn@ljnstudy.com>
2022-10-24 13:01:56 +0800

    first commit

---
+++ src/main/java/hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author ljnstudy
+ * @create 2022-10-24 下午 12:36
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello2");
+    }
+}
---
+++ src/main/java/hellogit.java
@@ -0,0 +1,9 @@
+/**
+ * @author ljnstudy
+ * @create 2022-10-24 下午 12:53
+ */
+public class hellogit {
+    public static void main(String[] args) {
+        System.out.println("hhh");
+    }
+}
